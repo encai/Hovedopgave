02ea4fd3775ce6ce8683641ddaa89ef0eda051c6
Larfyrt <dlarfort@gmail.com>
2016-12-05 13:28:34 +0100

    Påbegyndt Test
    Tilføjet Xunit library mm.

---
+++ TestAdminCoreBackup/UnitTest/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using TestAdminCore;
+using TestAdminCore.Controllers;
+using TestAdminCore.Models;
+
+namespace UnitTest
+{
+    public class Class1
+    {
+        private ApplicationUser user = new ApplicationUser();
+        private ApplicationUser user1 = new ApplicationUser();
+        // ask about using lsit for db, and therefore you can test even more!
+        
+        [Fact]
+        public void TestApplicationUser()
+        {
+            user.FirstName = "John";
+            user.LastName = "Cena";
+            user1.FirstName = "Teddy";
+            user1.LastName = "Beer";
+            Assert.NotEqual(user, user1);
+            
+          
+        }
+    }
+}
