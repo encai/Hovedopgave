34269cb61ed0ac2dec8646de0400ef23f0b863e9
Larfyrt <dlarfort@gmail.com>
2016-12-08 14:37:56 +0100

    updates

--- TestAdminCoreBackup/src/xUnitTests/Class1.cs
+++ TestAdminCoreBackup/src/xUnitTests/Class1.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace xUnitTests
 {
     public class Class1
     {
-        public Class1()
+        [Fact]
+        public void PassingTest()
         {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
         }
     }
 }
